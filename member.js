b7f5c5577775489d678ad0dd000a97ae443a875f
CJRodr <160472019+CJRodr@users.noreply.github.com>
2024-05-16 15:09:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,33 @@
+function skillsMember() {
+
+    // Set the members skills
+    var skills = [
+        {
+            id: 0,
+            name: 'HTML',
+            level: 90
+        },
+        {
+            id: 1,
+            name: 'CSS',
+            level: 85
+        },
+        {
+            id: 2,
+            name: 'JavaScript',
+            level: 80
+        },
+        {
+            id: 3,
+            name: 'PHP',
+            level: 75
+        },
+        {
+            id: 4,
+            name: 'SQL',
+            level: 70
+        }
+    ];
+
+    return skills;
+}
